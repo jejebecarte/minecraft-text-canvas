d650722b70ed482496e358bd0e0ae4dc4f655dee
John Doe <69335633+Jejebecarte@users.noreply.github.com>
2022-08-02 09:52:35 +1000

    refactor: remove unncessary string replacement

--- src/func/setCanvasDimensions.ts
+++ src/func/setCanvasDimensions.ts
@@ -6,12 +6,12 @@ import wrapText from "./wrapText";
 export default function setCanvasDimensions(text: string, ctx: CanvasRenderingContext2D) {
 	// Wrap and strip the text of modifiers before finding it's height and width
 	const wrappedText = wrapText(text, ctx);
-	const unformattedText = wrappedText.replaceAll("&.", "");
-	const measurement = ctx.measureText(unformattedText);
+	const measurement = ctx.measureText(wrappedText);
+
+	const width = getFormattedWidth(wrappedText, ctx);
 	const height = measurement.actualBoundingBoxAscent + measurement.actualBoundingBoxDescent;
-	const width = getFormattedWidth(unformattedText, ctx);
 
 	// Set canvas dimensions and reset font to adjust sizing
+	ctx.canvas.width = width;
 	ctx.canvas.height = height + fontOffset;
-	ctx.canvas.width = width + fontOffset;
 }
