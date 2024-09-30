13e858259468c57d6fb6c55dd16356552f687023
jejebecarte <69335633+jejebecarte@users.noreply.github.com>
2024-09-30 20:44:25 +1000

    feat: add underline

--- src/lib/renderText.ts
+++ src/lib/renderText.ts
@@ -10,14 +10,16 @@ import {
     ITALIC_FONT,
 } from './constants';
 
+const getFillStyle = (color: number) => `#${color.toString(16)}`;
+
 export default function renderText(text: string, ctx: CanvasRenderingContext2D) {
     let cursorY = FONT_SIZE - BOLD_SHADOW_OFFSET;
 
     text.split(NEWLINE_REGEX).forEach((line) => {
-        // Reset all properties
-        let currentColor: number = TEXT_COLORS.f;
-        let currentShadowColor: number = TEXT_SHADOW_COLORS.f;
+        let textFill = getFillStyle(TEXT_COLORS.f);
+        let shadowFill = getFillStyle(TEXT_SHADOW_COLORS.f);
         let isBold = false;
+        let isUnderlined = false;
         let cursorX = 0;
         ctx.font = FONT;
 
@@ -28,17 +30,20 @@ export default function renderText(text: string, ctx: CanvasRenderingContext2D) 
             // Check if this character and the next are a formatting code
             if (nextChar && char === '&' && `${char}${nextChar}`.match(SUPPORTED_FORMAT_CODES)) {
                 if (nextChar in TEXT_COLORS) {
-                    currentColor = TEXT_COLORS[nextChar as HexDigit];
-                    currentShadowColor = TEXT_SHADOW_COLORS[nextChar as HexDigit];
+                    textFill = getFillStyle(TEXT_COLORS[nextChar as HexDigit]);
+                    shadowFill = getFillStyle(TEXT_SHADOW_COLORS[nextChar as HexDigit]);
                 } else if (nextChar === 'r') {
-                    currentColor = TEXT_COLORS.f;
-                    currentShadowColor = TEXT_SHADOW_COLORS.f;
+                    textFill = getFillStyle(TEXT_COLORS.f);
+                    shadowFill = getFillStyle(TEXT_SHADOW_COLORS.f);
                     isBold = false;
+                    isUnderlined = false;
                     ctx.font = FONT;
                 } else if (nextChar === 'l') {
                     isBold = true;
                 } else if (nextChar === 'o') {
                     ctx.font = ITALIC_FONT;
+                } else if (nextChar === 'n') {
+                    isUnderlined = true;
                 } else {
                     continue;
                 }
@@ -46,27 +51,52 @@ export default function renderText(text: string, ctx: CanvasRenderingContext2D) 
                 // Skip the next character as it is part of a modifying sequence
                 i += 1;
             } else {
+                let { width } = ctx.measureText(char);
+
+                // Add space for underlined first char
+                if (isUnderlined && cursorX === 0) {
+                    cursorX += BOLD_SHADOW_OFFSET;
+                }
+
                 const shadowX = cursorX + BOLD_SHADOW_OFFSET;
                 const shadowY = cursorY + BOLD_SHADOW_OFFSET;
 
                 // Draw shadow
-                ctx.fillStyle = `#${currentShadowColor.toString(16)}`;
+                ctx.fillStyle = shadowFill;
                 ctx.fillText(char, shadowX, shadowY);
                 if (isBold) {
                     ctx.fillText(char, shadowX + BOLD_SHADOW_OFFSET, shadowY);
                 }
 
                 // Draw text
-                ctx.fillStyle = `#${currentColor.toString(16)}`;
+                ctx.fillStyle = textFill;
                 ctx.fillText(char, cursorX, cursorY);
                 if (isBold) {
                     ctx.fillText(char, cursorX + BOLD_SHADOW_OFFSET, cursorY);
                 }
 
-                // Add extra space in between letters if character is bold
-                let { width } = ctx.measureText(char);
+                // Add extra spacing if character is bold
                 if (isBold) {
                     width += BOLD_SHADOW_OFFSET;
+                }
+
+                // Draw underline
+                if (isUnderlined) {
+                    ctx.fillStyle = shadowFill;
+                    ctx.fillRect(
+                        cursorX,
+                        cursorY + 2 * BOLD_SHADOW_OFFSET,
+                        width + BOLD_SHADOW_OFFSET,
+                        BOLD_SHADOW_OFFSET
+                    );
+
+                    ctx.fillStyle = textFill;
+                    ctx.fillRect(
+                        cursorX - BOLD_SHADOW_OFFSET,
+                        cursorY + BOLD_SHADOW_OFFSET,
+                        width + BOLD_SHADOW_OFFSET,
+                        BOLD_SHADOW_OFFSET
+                    );
                 }
 
                 cursorX += width;
--- src/lib/setCanvasDimensions.ts
+++ src/lib/setCanvasDimensions.ts
@@ -8,9 +8,6 @@ import {
     FONT,
 } from './constants';
 
-const BOLD_REGEX = /&l(.*?)(?:&r|$)/gm;
-const ITALIC_REGEX = /&o(.*?)(?:&r|$)/gm;
-
 // TODO: Maximum JPEG size is 65_535 x 65_535
 export default function setCanvasDimensions(text: string, ctx: CanvasRenderingContext2D) {
     const lines = text.split(NEWLINE_REGEX);
@@ -19,18 +16,17 @@ export default function setCanvasDimensions(text: string, ctx: CanvasRenderingCo
     // TODO: reuse the results from the bold and italic substring search in renderText() - draw text in chunks, not char by char
     lines.forEach((line) => {
         // Find the width of the line if no characters are bold or italic
-        // ? This appears to always be BOLD_SHADOW_OFFSET wider than the text really is
         ctx.font = FONT;
         let width = ctx.measureText(line.replaceAll(SUPPORTED_FORMAT_CODES, '')).width;
 
         // Add the extra width for bold substrings
-        line.match(BOLD_REGEX)?.forEach((subStr) => {
+        line.match(/&l(.*?)(?:&r|$)/gm)?.forEach((subStr) => {
             const modifiersRemoved = subStr.replaceAll(SUPPORTED_FORMAT_CODES, '');
             width += modifiersRemoved.length * BOLD_SHADOW_OFFSET;
         });
 
         // Add the extra width for italic substrings
-        [...line.matchAll(ITALIC_REGEX)]?.forEach((match) => {
+        [...line.matchAll(/&o(.*?)(?:&r|$)/gm)]?.forEach((match) => {
             const group = match[1]!;
 
             ctx.font = FONT;
@@ -39,7 +35,7 @@ export default function setCanvasDimensions(text: string, ctx: CanvasRenderingCo
             ctx.font = ITALIC_FONT;
             let italic = ctx.measureText(group.replaceAll(SUPPORTED_FORMAT_CODES, '')).width;
 
-            // To account for regular width naturally being BOLD_SHADOW_OFFSET wider than it really is, do the same for italic text at the end of the line
+            // If this is the end of the line, add the shadow width given italic text doesn't have a right bearing as the regular font does
             if (!match[0].endsWith('&r')) {
                 italic += BOLD_SHADOW_OFFSET;
             }
@@ -48,13 +44,26 @@ export default function setCanvasDimensions(text: string, ctx: CanvasRenderingCo
             width += italic - normal;
         });
 
+        // Add extra width for a starting underline
+        if (line.match(/^&n/m)) {
+            width += BOLD_SHADOW_OFFSET;
+        }
+
+        // Add extra width for an ending underline shadow
+        if (line.match(/&n(?:(?!&r).)*$/m)) {
+            width += BOLD_SHADOW_OFFSET;
+        }
+
         // If this is the widest line, set the overall width accordingly
         if (widestLineWidth < width) {
             widestLineWidth = width;
         }
     });
 
+    // Extra height for underline shadow on the bottom line
+    const underlineExtraheight = lines[lines.length - 1]?.includes('&n') ? BOLD_SHADOW_OFFSET : 0;
+
     // Add the shadow size to the height so it isn't cut off
-    ctx.canvas.height = FONT_SIZE * lines.length + BOLD_SHADOW_OFFSET;
+    ctx.canvas.height = FONT_SIZE * lines.length + BOLD_SHADOW_OFFSET + underlineExtraheight;
     ctx.canvas.width = widestLineWidth;
 }
